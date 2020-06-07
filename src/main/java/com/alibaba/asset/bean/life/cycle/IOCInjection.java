10285ca13aa5a1845199c50f2cdd6a41c4a7b99b
john <lrc272317227@gmail.com>
2020-06-07 10:16:02 +0800

    依赖注入

---
+++ src/main/java/com/alibaba/asset/bean/life/cycle/IOCInjection.java
@@ -0,0 +1,38 @@
+package com.alibaba.asset.bean.life.cycle;
+
+import lombok.Data;
+
+/**
+ * 老的注入方式
+ */
+public class IOCInjection {
+
+    @Data
+    static class HandsomeBody{
+        private BeautifulGirl girlFirend;
+
+        public void engage(){
+            System.out.println("I am engaging with " + girlFirend.getName());
+        }
+    }
+
+    @Data
+    static class BeautifulGirl{
+        private String name;
+        private String requirement;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("============== 老的注入方式  ============");
+        //先实例化
+        HandsomeBody steven = new HandsomeBody();
+        //实例化自己需要的属性
+        BeautifulGirl alice = new BeautifulGirl();
+        //设置各类属性
+        alice.setName("Alice");
+        alice.setRequirement("I want my boyfriend is tall, rich, handsome");
+        steven.setGirlFirend(alice);
+        //开始使用
+        steven.engage();
+    }
+}
