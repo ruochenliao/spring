a35c2fb7b3a2da48cd5f080a45beb155affd7e23
john <lrc272317227@gmail.com>
2020-06-07 09:54:13 +0800

    bean 的生命周期

--- src/main/java/com/alibaba/asset/bean/life/cycle/BeanLife.java
+++ src/main/java/com/alibaba/asset/bean/life/cycle/BeanLife.java
@@ -2,31 +2,57 @@ package com.alibaba.asset.bean.life.cycle;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.*;
+import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
 
-@Resource
+/**
+ * 在 bean 创建过程中，能够串改 bean 的名字，设置 bean 的创建工厂
+ *
+① 通过构造器或工厂方法创建bean实例
+② 为bean的属性设置值和对其他bean的引用
+③ 调用bean的初始化方法
+④ bean可以使用了
+⑤ 当容器关闭时，调用bean的销毁方法
+ */
+@Component
 public class BeanLife implements BeanNameAware, BeanFactoryAware, InitializingBean, DisposableBean {
     private String name;
 
-    @Override
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        System.out.println("init beanLife factory");
-    }
-
+    /**
+     * bean 的名称
+     * @param s
+     */
     @Override
     public void setBeanName(String s) {
         this.name = s;
         System.out.println("set bean name :" + s);
     }
 
+    /**
+     *
+     * @param beanFactory
+     * @throws BeansException
+     */
     @Override
-    public void destroy() throws Exception {
-        System.out.println("destroy bean");
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("init beanLife factory");
     }
 
+    /**
+     * 属性注入
+     * @throws Exception
+     */
     @Override
     public void afterPropertiesSet() throws Exception {
         System.out.println("after properties are set");
+    }
+
+    /**
+     * 销毁
+     * @throws Exception
+     */
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("destroy bean");
     }
 }
--- src/main/java/com/alibaba/asset/bean/life/cycle/Processor.java
+++ src/main/java/com/alibaba/asset/bean/life/cycle/Processor.java
@@ -2,20 +2,21 @@ package com.alibaba.asset.bean.life.cycle;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
 /**
+ * bean 初始化之后可以做这些事情
+ *
  * @author john
  */
-@Resource
+@Component
 public class Processor implements BeanPostProcessor {
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         if(bean instanceof BeanLife){
-            System.out.println("before init " + beanName + " created!");
-        }else{
-            System.out.println("initing beans");
+            System.out.println("before init " + bean.getClass().getSimpleName() + " created!");
         }
         return bean;
     }
